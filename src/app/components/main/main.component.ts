a7eaac121c696723b112a3f5a7e8ef0c3f5d1684
aydin <aydin@aydin.can>
2019-10-22 10:19:32 +0300

    latest

--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -23,17 +23,27 @@ export class MainComponent implements OnInit {
       .pipe(
         debounceTime(500),
         filter(text => text.length >= 3),
-        switchMap(this.getMovies)
+        tap(curr => console.log(curr)),
+        switchMap(this.getMovies),
+        map((elem: any) => ({
+          ...elem,
+          results: elem.results.map((item) => ({
+            ...item,
+            poster_path: item.poster_path ? 'http://image.tmdb.org/t/p/w185/' + item.poster_path : null
+          }))
+        }))
       )
       .subscribe((res: any) => {
-        if (res.Response == 'False') {
+        console.log(res);
+        if (res.results == []) {
           this.movies = [];
-          this.errorMessage = res.Error
+          this.errorMessage = res.Error;
         }
-        const search = res.Search;
-        this.errorMessage = "";
+        const search = res.results;
+        this.errorMessage = '';
         this.movies = search;
-      })
+        console.log(this.movies);
+      });
   }
 
   getMovies = (text) => this.movieservice.getAllMovies(text);
--- src/app/services/movie.service.ts
+++ src/app/services/movie.service.ts
@@ -9,6 +9,6 @@ export class MovieService {
   constructor(private http: HttpClient) { }
   
   getAllMovies(title:string){
-    return this.http.get(`http://www.omdbapi.com/?apikey=9c1059c9&s=${title}`);
+    return this.http.get(`http://api.themoviedb.org/3/search/movie?api_key=931bdd8351ce1aff8e07cbf500429f27&language=en-US&page=1&include_adult=false&query=${title}`);
   }
 }
